884433aa3161cab623d85a9aeb6e555ccedc276e
Juan Ignacio Martel Sanchez <juano@MacBook-Pro-de-Juan.local>
2019-07-03 15:06:15 -0300

    Factorizacion de codigo

--- module/Application/src/Service/MovimientosManager.php
+++ module/Application/src/Service/MovimientosManager.php
@@ -22,7 +22,6 @@ class MovimientosManager {
     public function procesarArchivoExcel($archivo){
         $spreadsheet = IOFactory::load($archivo);
         $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-        // var_dump($sheetData);
 
         foreach ($sheetData as $filaDatos){
             $this->procesarFilasArchivo($filaDatos);
@@ -46,23 +45,18 @@ class MovimientosManager {
     }
 
     /**
-     * Como esta compuesta la fila de datos?
-     * 'A' => Fecha del movimiento
-     * 'B' => Sucursal del banco
-     * 'C' => Detalle del movimiento (dividir en dos partes)
-     * 'D' => Un codigo
-     * 'E' => Monto del movimiento
+     * Procesa la descripcion del movimiento que viene en el archivo.
+     * Lo ideal seria separarlo en dos: MotivoMovimiento y Detalle del Movimiento.
+     * 
+     * En algunos casos, solo viene con MotivoMovimiento y en otro se fuerza la separación
+     * detectando con qué tarjeta se hizo el movimiento.
      *
-     * @param [type] $filaDatos
-     * @return void
+     * @param [String] $detalle
+     * @return array
      */
-    private function procesarFilasArchivo($filaDatos){
-        $fechaOriginal = $filaDatos['A'];
-        $detalle = $filaDatos['C'];
-        $valor = $filaDatos['E'];
-
+    private function procesarDetalleMovimientoDesdeArchivo($detalle){
         $nuevoString = '';
-        //Procesamiento del detalle del movimiento
+        
         if (preg_match("/\s\s+/", $detalle)) {
             //Si tiene mas de dos espacios consecutivos, entonces es 
             //MotivoMovimiento - Detalle del Movimiento (Local o tarjeta)
@@ -83,33 +77,67 @@ class MovimientosManager {
         //[1] => Detalle del movimiento
         //[2] => '' --> Ocasionalmente
         $arrStrings = explode('@', $nuevoString);
+        return $arrString;
+    }
+
+    /**
+     * Como esta compuesta la fila de datos?
+     * 'A' => Fecha del movimiento
+     * 'B' => Sucursal del banco
+     * 'C' => Detalle del movimiento (dividir en dos partes)
+     * 'D' => Un codigo
+     * 'E' => Monto del movimiento
+     *
+     * @param [array] $filaDatos
+     * @return void
+     */
+    private function procesarFilasArchivo($filaDatos){
+        $fechaOriginal = $filaDatos['A'];
+        $detalle = $filaDatos['C'];
+        $valor = $filaDatos['E'];
+        //Procesamiento del detalle del movimiento
+        $arrStrings = $this->procesarDetalleMovimientoDesdeArchivo($detalle);
 
         if (count($arrStrings) > 1){
             $motivoMovimiento = $this->catalogoManager->getMotivoMovimientoPorDescripcion($arrStrings[0]);
 
             if (!isset($motivoMovimiento)){
-                $motivoMovimiento = new MotivoMovimiento();
-                $motivoMovimiento->setDescripcion(trim($arrStrings[0]));
-
-                $this->entityManager->persist($motivoMovimiento);
-                $this->entityManager->flush();
+                $motivoMovimiento = $this->nuevoMotivoMovimiento($arrStrings[0]);
             }
 
             $formato = 'd/m/Y H:i';
             $fecha = \DateTime::createFromFormat($formato, $fechaOriginal);
             
-            $Movimiento = new Movimiento();
-            $Movimiento->setFecha($fecha->format('Y-m-d\TH:i:s'));
-            $Movimiento->setMotivoMovimiento($motivoMovimiento);
-            $Movimiento->setValor($valor);
-            $Movimiento->setDescripcion(trim($arrStrings[1]));
-
-            $this->entityManager->persist($Movimiento);
-            $this->entityManager->flush();
+            $this->nuevoMovimiento($fecha, $motivoMovimiento, $descripcion, $valor);
         }else{
             //Fallo al reconocer el movimiento
             return null;
         }
+    }
+
+    //Se pueden mover al manager de catalogo lo que sigue de acá para abajo
+
+    public function nuevoMotivoMovimiento($descripcion){
+        $MotivoMovimiento = new MotivoMovimiento();
+        $MotivoMovimiento->setDescripcion(trim($descripcion));
+
+        $this->entityManager->persist($MotivoMovimiento);
+        $this->entityManager->flush();
+
+        return $MotivoMovimiento;
+    }
+
+    public function nuevoMovimiento($fecha, $MotivoMovimiento, $descripcion, $valor){
+        $Movimiento = new Movimiento();
+        $Movimiento->setFecha($fecha->format('Y-m-d\TH:i:s'));
+        $Movimiento->setMotivoMovimiento($MotivoMovimiento);
+        $Movimiento->setDescripcion(trim($descripcion));
+        $Movimiento->setValor($valor);
+
+        $this->entityManager->persist($Movimiento);
+        $this->entityManager->flush();
+
+        return $Movimiento;
     }
 
     public function actualizarCategoriaDeMotivoMovimiento($MotivoMovimiento, $IdCategoriaMov){
